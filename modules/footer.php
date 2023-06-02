79849ba9468ac328a44c00cbdccf46ae156da0ef
Juan Antonio Hernandez Marquez <juanantoniohernandezmarquez01@gmail.com>
2023-06-02 08:18:00 -0600

    se modifica la imagen del footer

--- modules/footer.php
+++ modules/footer.php
@@ -1,4 +1,7 @@
 <div class="container py-4">
     <p>&copy; 2023 Mi Sitio. Todos los derechos reservados. | Diseñado por Ing. Juan Antonio Hernández Márquez
-         <a href="https://www.linkedin.com/in/juan-antonio-hern%C3%A1ndez-m%C3%A1rquez-770635264/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/38/38669.png" alt="LinkedIn"></a></p>
-  </div>
+        <a href="https://www.linkedin.com/in/juan-antonio-hern%C3%A1ndez-m%C3%A1rquez-770635264/" target="_blank">
+            <img src="https://cdn-icons-png.flaticon.com/512/38/38669.png" alt="LinkedIn" width="20" height="20">
+        </a>
+    </p>
+</div>
