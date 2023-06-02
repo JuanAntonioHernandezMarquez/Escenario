f101849f7edb86af42d956b8f02160d4a8537c57
Juan Antonio Hernandez Marquez <juanantoniohernandezmarquez01@gmail.com>
2023-06-02 06:44:51 -0600

    se agrega el haeder y el footer en el index usando boostrap

---
+++ modules/footer.php
@@ -0,0 +1,4 @@
+<div class="container py-4">
+    <p>&copy; 2023 Mi Sitio. Todos los derechos reservados. | Diseñado por Ing. Juan Antonio Hernández Márquez
+         <a href="https://www.linkedin.com/in/juan-antonio-hern%C3%A1ndez-m%C3%A1rquez-770635264/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/38/38669.png" alt="LinkedIn"></a></p>
+  </div>
