4ece4064fcd9f724a4373e656b6e06d1ac43c90a
Juan Antonio Hernandez Marquez <juanantoniohernandezmarquez01@gmail.com>
2023-06-02 08:41:44 -0600

    se modifica el index

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Escena con Modelado 3D y movimiento con Teclas</title>
+    <title>Escena con Modelado 3D</title>
     <style>
         body {
             margin: 0;
         }
     </style>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 
 <body>
-    <?php include("modules/header.php"); ?>
-
+    <?php include("modules/header.php"); ?> 
     <script src="js/three.js"></script>
-    <script src="js/ObjectLoader.js"></script>
-    <script src="js/OBJLoader.js"></script>
+    <script src="js/ObjectLoader.js"></script> <!-- Se requiere OBJLoader.js para cargar el modelo OBJ -->
     <script>
+     
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -29,7 +27,7 @@
         document.body.appendChild(renderer.domElement);
 
         // Creamos un cargador para cargar el modelo OBJ
-        const loader = new THREE.OBJLoader2();
+        const loader = new THREE.OBJLoader();
 
         let object; // Variable para almacenar el modelo OBJ
 
@@ -78,13 +76,13 @@
         }
         animate();
     </script>
-
-<?php include("modules/footer.php"); ?>
-    <!--JavaScript de Bootstrap -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     <?php include("modules/footer.php"); ?> 
 </body>
 
 </html>
 
+   
+
+   
+   
+  
